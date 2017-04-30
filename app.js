c3969a0f18d8df197c0f1e86fccc2b990ecaecf0
himangkydutta <himangkydutta08@gmail.com>
2017-04-30 20:04:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//JavaScript loops
+var sum = 0;
+
+/*
+//Here we are using for loop to print sum of 1000
+for(var i=1;i<=1000;i++)
+{
+    sum=sum+i;
+    console.log("Sum of using for-loop is :-"+ sum);
+}
+*/
+
+//Here we are using while loop to print sum of 1000
+var i = 1;
+while(i<=1000)
+{
+    sum+=i;
+    console.log("Sum of using while-loop is :-"+ sum);
+    i++;
+}
+
+	
